752de863cc4e3328526f84cf8076320501d3e8b9
Thomás <86493097+tminhas@users.noreply.github.com>
2024-05-20 18:06:54 -0300

    Delete projeto.py

--- projeto.py
+++
@@ -1,7 +0,0 @@
-class Projeto:
-    def __init__(self, nome, funcionarios):
-        self.funcionarios = funcionarios
-        self.nome = nome
-
-    def adiciona_funcionario(self, funcionario):
-        self.funcionarios.append(funcionario)
